62713f86f59cfc7c4cd3cc3dff17309428c9a3dc
Sasha <doom@ya.ru>
2018-04-20 22:15:10 +0300

    New functional + refactoring

--- IML-AT-Core/Core/DriverFactory.cs
+++ IML-AT-Core/Core/DriverFactory.cs
@@ -6,13 +6,16 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Opera;
 using OpenQA.Selenium.Safari;
+using IML_AT_Core.Extensions;
+using OpenQA.Selenium.Support.Events;
+using OpenQA.Selenium.Support.Extensions;
+
 
 namespace IML_AT_Core.Core
 {
     public abstract class DriverFactory
     {
-        private static ThreadLocal<IWebDriver> _driver = new ThreadLocal<IWebDriver>();
-
+        private static ThreadLocal<IWebDriver> _driver;
         public static IWebDriver GetDriver()
         {
             if (!_driver.IsValueCreated)
@@ -21,40 +24,81 @@ namespace IML_AT_Core.Core
             return _driver.Value;
         }
 
+
         public static void InitDriver(Browser browserType)
-        {
+        {if (_driver == null) _driver = new ThreadLocal<IWebDriver>();
             switch (browserType)
+
             {
-                case Browser.Chrome:
+                case Browser.chrome:
+
                     _driver.Value = new ChromeDriver();
+
                     break;
-                case Browser.Firefox:
+
+                case Browser.firefox:
+
                     _driver.Value = new FirefoxDriver();
+
                     break;
-                case Browser.InternetExplorer:
+
+                case Browser.ie:
+
                     _driver.Value = new InternetExplorerDriver();
+
                     break;
-                case Browser.Safari:
+
+                case Browser.safari:
+
                     _driver.Value = new SafariDriver();
+
                     break;
-                case Browser.Opera:
+
+                case Browser.opera:
+
                     _driver.Value = new OperaDriver();
+
                     break;
+
                 default:
+
                     throw new ArgumentNullException("Неизвестный тип драйвера \"" + browserType +
                                                     "\". Невозможно проинициализировать драйвер.");
             }
 
+
             _driver.Value.Manage().Window.Maximize();
+
             _driver.Value.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            var firingDriver = new EventFiringWebDriver(_driver.Value);
+
+            _driver.Value = firingDriver;
+
+            firingDriver.ExceptionThrown += FiringDriver_ExceptionThrown;
         }
 
+
+        private static void FiringDriver_ExceptionThrown(object sender, WebDriverExceptionEventArgs e)
+
+        {
+            // string timestamp = DateTime.Now.ToString("dd-MM-yyyy-hhmmss");
+
+            //   _driver.Value.TakeScreenshot().SaveAsFile(timestamp + ".png", ScreenshotImageFormat.Png);
+        }
+
+
         public static void Dispose()
+
         {
             if (_driver.Value == null && _driver == null) return;
-            _driver.Value.Close();
+
+            _driver.Value.Quit();
+
             _driver.Value = null;
+
             _driver.Dispose();
+
             _driver = null;
         }
     }
--- IML-AT-Core/Extensions/WaitExtensions/Interfaces/IPageWaitConditions.cs
+++ IML-AT-Core/Extensions/WaitExtensions/Interfaces/IPageWaitConditions.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IML_AT_Core.Extensions.WaitExtensions.Interfaces
+﻿namespace IML_AT_Core.Extensions.WaitExtensions.Interfaces
 {
     public interface IPageWaitConditions
     {
         void TitleEqual(string title);
         void UrlEqual(string url);
-        void UrlContain(string url);
+        bool UrlContain(string url);
         void UrlMatches(string regex);
-        void ReadyStateComplete();
+        bool ReadyStateComplete();
         void TitleContain(string title, bool ignoreCase = false);
         void LoaderDissapear();
     }
