e9b6d3f524d536e444fcbba348f373e6493ecfb7
Sasha <doom@ya.ru>
2018-04-29 13:18:26 +0300

    restore project

---
+++ TestsExamples/Pages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsExamples
+{
+    public static class Pages<T>
+    {
+        public static TPage GetPage<TPage>(TPage page) where TPage: new ()
+        {
+            return page;
+        }
+    }
+}
---
+++ TestsExamples/Pages/MainPage.cs
@@ -0,0 +1,26 @@
+﻿using IML_AT_Core.CustomElements.Attributes;
+using IML_AT_Core.CustomElements.Elements;
+using IML_AT_Core.TestBaseClasses;
+using OpenQA.Selenium.Support.PageObjects;
+using TestsExamples.Blocks.Common;
+
+namespace TestsExamples.Pages
+{
+    public class MainPage : BasePage
+    {
+        [FindBy(XPath = "//a[@class = \'login\']")]
+        [ElementTitle("Войти")]
+        private ImlButton loginButton;
+        
+
+        private ContactInformationBlock contactInformation;
+
+        public LoginPage ClickOnLoginButton()
+        {
+            loginButton.Click();
+            return new LoginPage();
+        }
+        
+        
+    }
+}
