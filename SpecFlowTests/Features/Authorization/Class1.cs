389f4adca1583601d20af006eda29317a4aedeea
Александр Данилин <a.danilin@im-logistics.tld>
2018-07-04 12:56:48 +0300

    123

---
+++ SpecFlowTests/Features/Authorization/Class1.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace SpecFlowTests.Features.Authorization
+{
+    [Order(4)]
+    public partial class ПопыткаВойтиСНеправильнымЛогиномЛибоПаролемFeature
+    {
+    }
+}
